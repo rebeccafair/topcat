4a2819305c78893406917ab16bbe7e7a9866c635
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2016-06-29 13:20:40 +0100

    Enable correct display and submission of boolean group type job options

--- yo/app/scripts/controllers/configure-job.controller.js
+++ yo/app/scripts/controllers/configure-job.controller.js
@@ -14,6 +14,7 @@
         var cartContainsDatafiles = _.includes(cartEntityTypes, 'datafile');
         var multipleCartItems = cartItems.length > 1;
         var cartDatasetTypes = [];
+        var booleanGroupNames = [];
         this.numCartItems = cartItems.length;
         this.loadingJobTypes = true;
         this.loadingJobOptions = true;
@@ -43,18 +44,24 @@
         this.submitSingleJob = function() {
             var jobParameters = [];
 
-            _.each(this.selectedJobType.jobOptions, function (jobOption){
-                if (jobOption.value === true) {
-                    jobParameters.push(jobOption.programParameter);
-                  } else {
+            if (cartContainsDatafiles) jobParameters.push('--datafileIds=' + _.map(cartItems, 'entityId').join(','));
+            if (cartContainsDatasets) jobParameters.push('--datasetIds=' + _.map(cartItems, 'entityId').join(','));
+
+            _.each(that.selectedJobType.jobOptions, function (jobOption){
+                switch (jobOption.type) {
+                    case "boolean":
+                            if (jobOption.value === true) { jobParameters.push(String(jobOption.programParameter)) }
+                        break;
+                    case "booleanGroup":
+                        if (jobOption.value !== "") { jobParameters.push(jobOption.value) }
+                        break;
+                    default:
+                        if (jobOption.value !== "") {
                             jobParameters.push(jobOption.programParameter);
                             jobParameters.push(String(jobOption.value));
-                  }
+                        }
+                }
             });
-
-            if (cartContainsDatafiles) jobParameters.unshift('--datafileIds=' + _.map(cartItems, 'entityId').join(','));
-            if (cartContainsDatasets) jobParameters.unshift('--datasetIds=' + _.map(cartItems, 'entityId').join(','));
-
             tc.ijp(cartItems[0].facilityName).submitJob(this.selectedJobType.name, jobParameters);
 
             this.confirmModal.close('job submitted');
@@ -63,23 +70,29 @@
         };
 
         this.submitMultipleJobs = function() {
-            var jobOptionsParameters = [];
-            var jobParameters = [];
-
-            _.each(this.selectedJobType.jobOptions, function (jobOption){
-                if (jobOption.value === true) {
-                    jobOptionsParameters.push(jobOption.programParameter);
-                  } else {
-                            jobOptionsParameters.push(jobOption.programParameter);
-                            jobOptionsParameters.push(String(jobOption.value));
-                    }
-            });
 
             _.each(cartItems, function(cartItem) {
-                jobParameters = jobOptionsParameters;
-                if (cartItem.entityType === 'datafile') jobParameters.unshift('--datafileIds=' + cartItem.entityId);
-                if (cartItem.entityType === 'dataset') jobParameters.unshift('--datasetIds=' + cartItem.entityId);
+                var jobParameters = [];
 
+                if (cartItem.entityType === 'datafile') jobParameters.push('--datafileIds=' + cartItem.entityId);
+                if (cartItem.entityType === 'dataset') jobParameters.push('--datasetIds=' + cartItem.entityId);
+
+                _.each(that.selectedJobType.jobOptions, function (jobOption){
+                    switch (jobOption.type) {
+                        case "boolean":
+                            if (jobOption.value === true) { jobParameters.push(jobOption.programParameter) }
+                            break;
+                        case "booleanGroup":
+                            if (jobOption.value !== "") { jobParameters.push(jobOption.value) }
+                            break;
+                        default:
+                            if (jobOption.value !== "") {
+                                jobParameters.push(jobOption.programParameter);
+                                jobParameters.push(jobOption.value);
+                            }
+                    }
+                });
+                console.log(jobParameters);
                 tc.ijp(cartItem.facilityName).submitJob(that.selectedJobType.name, jobParameters);
             });
 
@@ -87,19 +100,16 @@
             $uibModalInstance.close('job submitted');
         };
 
-        this.cancel = function() {
+        this.cancel = function(){
             $uibModalInstance.dismiss('cancel');
         };
 
-        this.back = function() {
+        this.back = function(){
             this.confirmModal.dismiss('back');
         };
 
-        this.jobTypeSelected = function() {
-            that.loadingJobOptions = true;
-            setupJobOptions();
+        this.jobTypeSelected = function(){
             that.form.$setPristine();
-            that.loadingJobOptions = false;
         }
 
         function getAllJobTypes(){
@@ -149,10 +159,9 @@
                         that.selectedJobType = that.compatibleJobTypes[0] || "";
                         that.loadingJobTypes = false;
                         setupJobOptions();
-                        that.loadingJobOptions = false;
                     });
 
-                } else {
+                } else if (cartContainsDatafiles) {
                     //If the cart doesn't contain datasets, it must contain datafiles. The job type must have acceptsDatafiles = true
                     var compatibleJobTypes = _.filter(allJobTypes, function(jobType) { return stringToBoolean(jobType.acceptsDatafiles) });
                     //EDIT: commented out for now, might want to run a job for each item in the cart, so don't need to filter by multiple
@@ -163,7 +172,15 @@
                     that.selectedJobType = compatibleJobTypes[0] || "";
                     that.loadingJobTypes = false;
                     setupJobOptions();
-                    that.loadingJobOptions = false;
+
+                } else {
+                    //If there is no input, show job-only jobs, where datasetTypes = []
+                    var compatibleJobTypes = _.filter(allJobTypes, function(jobType) { return !(jobType.datasetTypes.length > 0) });
+
+                    that.compatibleJobTypes = compatibleJobTypes;
+                    that.selectedJobType = compatibleJobTypes[0] || "";
+                    that.loadingJobTypes = false;
+                    setupJobOptions();
                 }
 
             });
@@ -194,24 +211,53 @@
         }
 
         function setupJobOptions(){
+            _.each(that.compatibleJobTypes, function(jobType){
+                //Change structure of any 'boolean group' job options so they are more easily constructible in html
+                //Find unique boolean group names
+                var groupNames = _.uniq(_.filter(_.map(jobType.jobOptions, 'groupName'), undefined));
+                _.each(groupNames, function(groupName){
+                    //Find first option associated with that group, replace it with a single object containing all option information for that group,
+                    //and remove all following objects associated with that group.
+                    var firstGroupMember = _.findIndex(jobType.jobOptions, function(option) { return option.groupName === groupName});
+                    var newJobOption = {
+                        groupName: groupName,
+                        type: "booleanGroup",
+                        programParameter: "",
+                        values: []
+                    };
+                    _.each(_.filter(jobType.jobOptions, function(option){
+                        return option.groupName === groupName;
+                    }), function(groupJobOption) {
+                        newJobOption.values.push({
+                            name: groupJobOption.name,
+                            programParameter: groupJobOption.programParameter
+                        });
+                    });
+                    jobType.jobOptions[firstGroupMember] = newJobOption;
+                    _.remove(jobType.jobOptions, function(option) { return option.groupName === groupName && option.type !== 'booleanGroup' });
+                });
 
-            _.each(that.selectedJobType.jobOptions, function(option){
-
-                //Set up default values
-                switch (option.type) {
-                    case "boolean":
-                        option.value = stringToBoolean(option.defaultValue) || false;
-                        break;
-                    case "integer":
-                    case "float":
-                        option.value = isNaN(parseFloat(option.defaultValue)) ? "" : parseFloat(option.defaultValue);
-                        break;
-                    case "string":
-                        option.value = option.defaultValue || "";
-                        break;
-                    case "enumeration":
-                        option.value = option.defaultValue || option.values[0];
-                } 
+                _.each(jobType.jobOptions, function(option){
+                    //Set up default values
+                    switch (option.type) {
+                        case "boolean":
+                            option.value = stringToBoolean(option.defaultValue) || false;
+                            break;
+                        case "integer":
+                        case "float":
+                            //isNaN check required instead of just ||, because option.defaultValue = false if defaultValue = 0
+                            option.value = isNaN(parseFloat(option.defaultValue)) ? "" : parseFloat(option.defaultValue);
+                            break;
+                        case "string":
+                            option.value = option.defaultValue || "";
+                            break;
+                        case "enumeration":
+                            option.value = option.defaultValue || option.values[0];
+                            break;
+                        case "booleanGroup":
+                            option.value = option.values[0].programParameter;
+                    }
+                });
             });
         }
 
